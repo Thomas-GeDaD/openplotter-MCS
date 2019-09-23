a8053934e07d5cb20957c3b5db405338e2d23a1b
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2019-09-23 14:15:03 +0200

    Update MCSPostInstall.py

--- openplotterMCS/MCSPostInstall.py
+++ openplotterMCS/MCSPostInstall.py
@@ -42,22 +42,19 @@ def main():
 	except Exception as e: print(_('FAILED: ')+str(e))
 
 
-	####### /boot/config.txt edit
+	####### /boot/config.txt edit for SC16is752 and MCP2515
 
 	print(_('Adding config.txt entries...'))
 
 	try:
 		fo1r = open('/boot/config.txt', "r")
-
 		configcontent = fo1r.read()
 		startpos = configcontent.find("#MCS-Openplotter config (Do not delete or edit this part)(start)")
 		fo1r.close()
 		print(_(startpos))
 		
-		fo1 = open('/boot/config.txt', "a")
-
 		if (startpos == -1):
-
+			fo1 = open('/boot/config.txt', "a")
 			fo1.write ("\n\n\n")
 			fo1.write ("#MCS-Openplotter config (Do not delete or edit this part)(start)\n")
 			fo1.write ("dtoverlay=mcp2515-can1,oscillator=16000000,interrupt=25\n")
@@ -71,8 +68,29 @@ def main():
 		print(_("config.txt entries created"))
 
 	except Exception as e: print(_('FAILED: ')+str(e))
-
-
+	
+	####### add CAN Interface for MCP2515 CAN0
+	
+	try:
+		fo2r = open ("/etc/network/interfaces", "r")
+		interfacecontent= fo2r.read()
+		intava = interfacecontent.find ("source /etc/network/interfaces.d/*")
+		fo2r.close()
+		print (_(intava))
+		
+		if (inava == -1)
+			fo2 = open ("/etc/network/interfaces", "a")
+			fo2.write = ("\nsource /etc/network/interfaces.d/")
+			fo2.close()
+		print(_("interface.d appended"))
+		
+		fo3 = open ("/etc/network/interfaces/interfaces.d/can0" "w")
+		fo3.write ("#physical can interfaces\nallow-hotplug can0\niface can0 can static\nbitrate 250000\ndown /sbin/ip link set $IFACE down\nup /sbin/ifconfig $IFACE txqueuelen 10000")
+		fo3.close()
+		print(_("interface.d can0 created")
+		
+	except Exception as e: print(_('FAILED: ')+str(e))
+		
 if __name__ == '__main__':
 
 
