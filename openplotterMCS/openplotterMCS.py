92d0f0224153b7442882224ef023ff409b44329d
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2019-09-27 07:47:24 +0200

    Update openplotterMCS.py

--- openplotterMCS/openplotterMCS.py
+++ openplotterMCS/openplotterMCS.py
@@ -453,6 +453,7 @@ class addowire(wx.Dialog):
 
 		cancelBtn = wx.Button(panel, wx.ID_CANCEL)
 		okBtn = wx.Button(panel, wx.ID_OK)
+		refreshBtn = wx.Button(panel, self.refresh)
 
 		hbox3 = wx.BoxSizer(wx.HORIZONTAL)
 		hbox3.Add(label_SK_before, 0, wx.RIGHT | wx.LEFT | wx.EXPAND, 5)
@@ -461,8 +462,9 @@ class addowire(wx.Dialog):
 
 		hbox = wx.BoxSizer(wx.HORIZONTAL)
 		hbox.AddStretchSpacer(1)
+		hbox.Add(refreshBtn, 0, wx.ALL | wx.EXPAND, 5)
 		hbox.Add(cancelBtn, 0, wx.ALL | wx.EXPAND, 5)
-		hbox.Add(okBtn, 0, wx.ALL | wx.EXPAND, 5)
+		hbox.Add(okBtn, 0, wx.LEFT | wx.EXPAND, 5)
 
 		vbox = wx.BoxSizer(wx.VERTICAL)
 		vbox.AddSpacer(5)
@@ -480,6 +482,10 @@ class addowire(wx.Dialog):
 
 		#self.detection()
 		self.Centre() 
+		
+		###
+		def refresh (self,e)
+			print ("refresh")
 
 
 ################################################################################
