bae0ec089313ac7965e1902c90c99198fd551c90
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2019-10-10 15:57:16 +0200

    Update openplotterMCS.py

--- openplotterMCS/openplotterMCS.py
+++ openplotterMCS/openplotterMCS.py
@@ -639,7 +639,7 @@ class addowire(wx.Dialog):
 ################################################################################ New created owire
 class editowire(wx.Dialog):
 	def __init__(self,signalkkeys):
-		wx.Dialog.__init__(self, None, title=_('Edit 1-Wire Name'), size=(400,180))
+		wx.Dialog.__init__(self, None, title=_('Edit 1-Wire Name'), size=(400,220))
 		panel = wx.Panel(self)
 
 		Text1=wx.StaticText(panel, label=_('Enter new name for selected Sensor ID:'))
