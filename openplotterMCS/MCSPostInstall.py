aa503a3088e96b83011e72586a4262b76ca54226
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2019-10-15 14:11:59 +0200

    Update MCSPostInstall.py

--- openplotterMCS/MCSPostInstall.py
+++ openplotterMCS/MCSPostInstall.py
@@ -24,7 +24,7 @@ def main():
 	conf2 = conf.Conf()
 	currentdir = os.path.dirname(__file__)
 	currentLanguage = conf2.get('GENERAL', 'lang')
-	language.Language(currentdir,'openplotter-myapp',currentLanguage)
+	language.Language(currentdir,'openplotter-MCS',currentLanguage)
 
 	# here we create a service to run openplotter-MCS-read file
 
