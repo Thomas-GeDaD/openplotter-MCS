554238c6336e70f984c5e31f1b77f28f7b54dfbd
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2019-10-15 14:14:37 +0200

    Update version.py

--- openplotterMCS/version.py
+++ openplotterMCS/version.py
@@ -1,6 +1,6 @@
 version = '2.0'
 codeName = ''
-state = 'dev'
+state = 'beta'
 
 # version = First digit is the OpenPlotter version, second one is this app version. The debian package will have a third digit with the package version.
 # codeName = This is decided by the OpenPlotter team and contributors.
