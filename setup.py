c968e8a98efac997547c8692bb1df972d18898a5
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2019-12-02 12:57:06 +0100

    add autoshutdown entrypoint

--- setup.py
+++ setup.py
@@ -31,17 +31,11 @@ setup (
 	'Operating System :: POSIX :: Linux',
 	'Programming Language :: Python :: 3'],
 	include_package_data=True,
-	entry_points={'console_scripts': ['openplotter-MCS=openplotterMCS.openplotterMCS:main','openplotter-MCS-read=openplotterMCS.openplotterMCSRead:main','MCSPostInstall=openplotterMCS.MCSPostInstall:main','MCSPreUninstall=openplotterMCS.MCSPreUninstall:main']},
-	# entry_points: creating entry points you will be able to run these python scripts from everywhere.
-		# openplotter-myapp = This is the GUI of your app
-		# openplotter-myapp-read = You will use this file to be ran at startup as a service when needed using "sudo systemctl enable openplotter-myapp-read" in your code. See myappPostInstall file to see how to create services. If your script is a GUI script you need to start it in startup.py and not as a service.
-		# myappPostInstall = This file will be run just after package installation and it should contain any extra task to do like installing pip packages, creating services... If you are not using openplotter-settings to install this app (as for development), you will need to run myappPostInstall manually. 
-		# myappPreUninstall = This file will be run just before package uninstall. Here you should revert all changes in myappPostInstall.
+	entry_points={'console_scripts': ['openplotter-MCS=openplotterMCS.openplotterMCS:main','openplotter-MCS-read=openplotterMCS.openplotterMCSRead:main','openplotter-MCS-asd=openplotterMCS.openplotterMCSasd:main','MCSPostInstall=openplotterMCS.MCSPostInstall:main','MCSPreUninstall=openplotterMCS.MCSPreUninstall:main']},
+	
+	
 	scripts=['bin/myScript'],
-	# scripts: if you need to create entry points for python and non-python scripts like shell scripts you can use this.
-		# myScript = a shell script
-		# myappPostInstall = if your myappPostInstall file has to be a non-python script, put it in "scripts" and not in "entry_points".
-		# myappPreUninstall = if your myappPreUninstall file has to be a non-python script, put it in "scripts" and not in "entry_points".
+	
 	data_files=[('share/applications', ['openplotterMCS/data/openplotter-MCS.desktop']),('share/pixmaps', ['openplotterMCS/data/openplotter-MCS.png']),],
 	# data_files = Add files to the host system. This will work only when installed as debian package, not as a python package.
 	)
