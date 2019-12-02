773d4ef4b4708b5e5da310956996f758ca810e0d
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2019-12-02 12:59:37 +0100

    Create autoshutdown

--- openplotterMCS/openplotterMCSasd.py
+++ openplotterMCS/openplotterMCSasd.py
@@ -30,22 +30,17 @@ def main():
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(5, GPIO.IN)
 		start=GPIO.input(5)
-		print ("asd dienst gestartet")
-		print (enable, start)
 		
 		
-		if enable and start ="1":
-
+		if start ==1 and enable==True:
 			while True:
-				print("asd im while")
-				if GPIO.input(5) == "0" 
+				if GPIO.input(5) == "0" :
+					print ("shutdown in 2s")
 					time.sleep(2)
-					if GPIO.input(5) == "0"
+					if GPIO.input(5) == "0":
 						#os.system("sudo shutdown -h now")
 						os.system("sudo reboot")
-				
-				
-			time.sleep(2)
+				time.sleep(2)
 	except Exception as e: print (str(e))
 
 if __name__ == '__main__':
