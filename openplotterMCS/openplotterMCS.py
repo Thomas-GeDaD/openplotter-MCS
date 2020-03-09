d7bf7f41d13f74262b16408fb735f0a6d060808d
Thomas <54798285+Thomas-GeDaD@users.noreply.github.com>
2020-03-09 21:36:15 +0100

    Update for OP 2.2.0

--- openplotterMCS/openplotterMCS.py
+++ openplotterMCS/openplotterMCS.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Openplotter. If not, see <http://www.gnu.org/licenses/>.
 
-import wx, os, webbrowser, subprocess, socket
+import wx, os, webbrowser, subprocess, socket, time
 import wx.richtext as rt
 from openplotterSettings import conf
 from openplotterSettings import language
